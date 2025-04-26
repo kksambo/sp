6e84174756a3899697cc505f5480d636c0d60674
sicelo <124036348+kksambo@users.noreply.github.com>
2025-04-27 01:05:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -329,4 +330,5 @@ def get_user():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
